3fed982ed07b40b986197bdb91563ffcc9ae9726
Anant Tripathi <gammadeveloper5114@gmail.com>
2025-10-24 21:28:40 +0530

    added ts

---
+++ main.ts
@@ -0,0 +1,5 @@
+const display = document.querySelector(".display");
+const numberbuttons = document.querySelectorAll(".numpad button");
+const operatorbuttons = document.querySelectorAll(".operator button");
+numberbuttons[0].addEventListener("click",()=>{console.log("1")});
+
